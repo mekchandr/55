229bf3c3746d38f8a93f4995abc9d18589807832
mekchandr <35653274+mekchandr@users.noreply.github.com>
2018-03-13 15:59:39 +0530

    Create odd.c

---
+++ odd.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void main()
+
+{
+
+    int i,a =20,b=40;
+
+    printf("Print Odd Numbers in a given range a to b:\n");
+    
+    for (i = a; i <= b; i++)
+
+        {
+
+       if (i % 2 == 1)
+
+      printf ("%d ", i);
+
+}
+
+}
